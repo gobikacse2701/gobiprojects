4d15f925b3086a31570bc257cbb529770ea94bf0
gobikacse2701 <113690397+gobikacse2701@users.noreply.github.com>
2024-10-12 10:18:12 +0530

    Add files via upload

---
+++ java- practies/next.java
@@ -0,0 +1,17 @@
+
+class main{
+    public static void main(String args[] ){
+        for (int i=1;i<=1;i=i=1)
+        {
+            System.out.println(i);
+        }
+        int j=1;
+        while (j<=10)
+        {
+            System.out.println(j);
+            j=j+1;
+
+
+        }
+    }
+}
